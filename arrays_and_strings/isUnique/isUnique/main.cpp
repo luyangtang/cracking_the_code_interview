671473ee592980cfc452f335211126b3d6d1e095
luyangtang <luyang.tang@lmh.ox.ac.uk>
2019-01-08 22:08:14 +0000

    add merge function for merge sort

--- arrays_and_strings/isUnique/isUnique/main.cpp
+++ arrays_and_strings/isUnique/isUnique/main.cpp
@@ -7,14 +7,77 @@
 //
 
 #include <iostream>
-#include "mySolution.h"
+//#include "mySolution.h"
 
 
 
 
 int main() {
+    
+    int a[] = {1,2,6,7,8};
+    int sizeA = 5;
+    int b[] = {2,3,6,10,14,15};
+    int sizeB = 6;
+    int fullSize = sizeA + sizeB;
+    
+    // aInd and bInd are the running index for a and b
+    int aInd = 0; int bInd = 0;
+    // i is the index for the full array
+    int i = 0;
+    
+    // create an empty array for both
+    int *arr = new int[fullSize];
+    
+    while(aInd < sizeA && bInd < sizeB)
+    {
+        // compare current aInd and bInd
+        if (a[aInd] < b[bInd])
+        {
+            arr[i++] = a[aInd++];
+        }
+        else
+        {
+            arr[i++] = b[bInd++];
+        }
+        
+    }
+    
+    // last entry check
+    if (aInd == sizeA)
+    {
+        while (bInd < sizeB)
+        {
+            arr[i++] = b[bInd++];
+        }
+    }
+    else
+    {
+        while (aInd < sizeA)
+        {
+            arr[i++] = a[aInd++];
+        }
+    }
+        
+    
+    // see the result
+    for (int j = 0; j < fullSize; j++)
+    {
+        std::cout << arr[j] << ' ';
+    }
+    
+    
+    delete[] arr;
+    
     // define char const *string
-    testIsUnique();
+//    char string[] = "Hello world!";
+//
+//    int i = 0;
+//    while(string[i] != '\0')
+//    {
+//        std::cout << static_cast<int>(string[i]) << ' ';
+//        i++;
+//    }
+    
     return 0;
     
     
---
+++ arrays_and_strings/mergeSort/mergeSort/main.cpp
@@ -0,0 +1,117 @@
+//
+//  main.cpp
+//  mergeSort
+//
+//  Created by LYT on 08/01/2019.
+//  Copyright © 2019 Kiki Tan. All rights reserved.
+//
+
+#include <iostream>
+
+int* merge(int *a, int *b, int sizeA, int sizeB)
+{
+    int fullSize = sizeA + sizeB;
+    
+    // aInd and bInd are the running index for a and b
+    int aInd = 0; int bInd = 0;
+    // i is the index for the full array
+    int i = 0;
+    
+    // create an empty array for both
+    int *arr = new int[fullSize];
+    
+    while(aInd < sizeA && bInd < sizeB)
+    {
+        // compare current aInd and bInd
+        if (a[aInd] < b[bInd])
+        {
+            arr[i++] = a[aInd++];
+        }
+        else
+        {
+            arr[i++] = b[bInd++];
+        }
+        
+    }
+    
+    // last entry check
+    if (aInd == sizeA)
+    {
+        // if b has remaining entries then populate it to arr
+        while (bInd < sizeB)
+        {
+            arr[i++] = b[bInd++];
+        }
+    }
+    else
+    {
+        // if a has remaining entries then populate it to arr
+        while (aInd < sizeA)
+        {
+            arr[i++] = a[aInd++];
+        }
+    }
+    
+    
+//    // see the result
+//    for (int j = 0; j < fullSize; j++)
+//    {
+//        std::cout << arr[j] << ' ';
+//    }
+    
+    int *merged = arr;
+    delete[] arr;
+    return merged;
+}
+
+
+void testMerge()
+{
+    int a[] = {1,2,6,7,8,100,240,560};
+    int b[] = {2,3,6,10,14,15};
+    int *arr = merge(a,b,sizeof(a)/sizeof(int),sizeof(b)/sizeof(int));
+    for (int i = 0; i < sizeof(a)/sizeof(int)+sizeof(b)/sizeof(int); i++)
+    {
+        std::cout << arr[i] << ' ';
+    }
+}
+
+int* mergeSort(int arr[], int arrSize)
+{
+    int midInd = arrSize / 2 + 1; // half of the length
+    int *a ; int *b;
+
+    if (arrSize == 1)
+    {
+        return arr;
+    }
+    else
+    {
+        // split the array into two
+        a = arr;
+        b = arr+midInd;
+        
+        // call recursion
+        int *sortedA = mergeSort(a, midInd);
+        int *sortedB = mergeSort(b, arrSize - midInd);
+        
+        // merge
+        return merge(sortedA, sortedB, midInd, arrSize - midInd);
+    }
+}
+
+
+int main() {
+    
+//    int arr[] = {94,146,28, 24,1,1, 9, 32,100000,3};
+//    int *sortedArr = mergeSort(arr, sizeof(arr)/sizeof(int));
+//
+//    for (int i = 0; i < sizeof(sortedArr)/sizeof(int); i++)
+//    {
+//        std::cout << sortedArr[i] << ' ';
+//    }
+    
+    testMerge();
+    
+    return 0;
+}
