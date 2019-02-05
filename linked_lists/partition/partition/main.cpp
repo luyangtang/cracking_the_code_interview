7b6e62f2855f831dae1c4b006fb805fea4dc94e9
luyangtang <kojika.tang@gmail.com>
2019-02-05 21:27:25 +0000

    (failed) recursive approach

--- linked_lists/partition/partition/main.cpp
+++ linked_lists/partition/partition/main.cpp
@@ -9,8 +9,57 @@
 #include <iostream>
 #include "mySolution.h"
 
+Node* partition2 (Node *head, int parVal)
+{
+    // if only one list then just return
+    if (head->next == nullptr)
+        return head;
+    else
+    {
+        // given a list use recursive calls to partition from the second element
+        Node *newHead = partition2(head->next, parVal);
+        
+        // then deal with the first element
+        // start from the second element
+        Node *end = newHead->next;
+        while (end->next!=nullptr)
+        {
+            end = end->next;
+        }
+        
+        // compare the head then send it to the back
+        if (head->data > parVal)
+        {
+            // update end
+            end->next = new Node(head->data);
+            end = end->next;
+            
+            // remove the head
+            Node *tmp = new Node(head->data);
+            head = head->next;
+            delete tmp;
+        }
+        else // keep it and link it to the rest
+        {
+            head->next = newHead;
+        }
+        return newHead;
+    }
+}
+
+void testPartition2()
+{
+    int arr[] = {0,1,2,3,4,5,6};
+    linkedList lst(arr,7);
+    partition2(lst.head,5);
+    lst.print();
+}
+
 int main()
 {
-    testPartition();
+    int arr[] = {3,5,8,5,10,2,1};
+    linkedList lst(arr,7);
+    partition2(lst.head,5);
+    lst.print();
     return 0;
 }
